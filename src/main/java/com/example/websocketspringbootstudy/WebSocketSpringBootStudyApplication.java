3ed86c4e77d0e9a3e6d4f97f766d9ef0f9083ddf
R4UL1N0 <raulino.storage@gmail.com>
2023-09-11 22:54:20 -0300

    initializing project with spring web & web socket libraries

---
+++ src/main/java/com/example/websocketspringbootstudy/WebSocketSpringBootStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocketspringbootstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketSpringBootStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketSpringBootStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/websocketspringbootstudy/WebSocketSpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocketspringbootstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketSpringBootStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
